5015b17f3b77d81e5d082aa12cb6bf031630352c
Raufur Rahman <rauf.rahmancz@gmail.com>
2020-06-07 22:06:22 +0200

    Update webpack.client.build.config.js

--- Chapter01/webpack.client.build.config.js
+++ Chapter01/webpack.client.build.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const buildDirectory = 'dist';
-const outputDirectory = buildDirectory + '/client';
+const outputDirectory = `${buildDirectory} + '/client'`;
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
